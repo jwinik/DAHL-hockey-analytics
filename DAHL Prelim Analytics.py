4a1cfd7da6aeec41a178ece8c4a244e32d04ea3e
jwinik <67568423+jwinik@users.noreply.github.com>
2021-09-08 17:49:02 -0500

    Adding EW Data

--- DAHL Prelim Analytics.py
+++ DAHL Prelim Analytics.py
@@ -8,8 +8,7 @@ from espn_api.hockey import League
 
 swid = "30DF2666-5ECF-459C-9F26-665ECF859C12"
 espn_s2 = 'AEAhXtbnZRQrhvOl6ptgNuOHeTnNWtlXJqvvTXfNFf0BgOjnNETLvKHqClqcorEn4%2F5fiybN1WKvlp1p2e5V7fRdvP9b551wjw%2FvlgS%2FvhZmjlHIsAzXhyyRKH%2BWIYXxmngLLec7UtqT242MwFbKjqJ%2Fm6a4lCqWa2fflj7x2WVlUt85e8muVAwNugG6rDpwBsNi%2BrcdOJks9ikbUrsSXp7wU5u7EBJBf7ZPlk57NnojcDWyr23TpOPXnqtiuDNAsBM8GuXJ7gy8neJacKOlQC5DZrQ33W0BuOWGKl%2F2V9GxCQ%3D%3D'
-username = 'jwinik96@gmail.com'
-pwd = 'Trekemondaalr69'
+
 
 league = League(league_id = 1140371907, year = 2021, espn_s2 = espn_s2, swid = swid, debug = True)
 
