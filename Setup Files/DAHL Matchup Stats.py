1838b206fc5dfa2dcd93a838a81e412d88c8fafd
jwinik <67568423+jwinik@users.noreply.github.com>
2022-04-03 12:32:47 -0500

    Added age function and plots

--- Setup Files/DAHL Matchup Stats.py
+++ Setup Files/DAHL Matchup Stats.py
@@ -16,7 +16,7 @@ from espn_api.hockey import League
 #espn_s2 = 'AEAhXtbnZRQrhvOl6ptgNuOHeTnNWtlXJqvvTXfNFf0BgOjnNETLvKHqClqcorEn4%2F5fiybN1WKvlp1p2e5V7fRdvP9b551wjw%2FvlgS%2FvhZmjlHIsAzXhyyRKH%2BWIYXxmngLLec7UtqT242MwFbKjqJ%2Fm6a4lCqWa2fflj7x2WVlUt85e8muVAwNugG6rDpwBsNi%2BrcdOJks9ikbUrsSXp7wU5u7EBJBf7ZPlk57NnojcDWyr23TpOPXnqtiuDNAsBM8GuXJ7gy8neJacKOlQC5DZrQ33W0BuOWGKl%2F2V9GxCQ%3D%3D'
 #league_id = 1140371907
 
-league = League(league_id = league_id, year = 2022, espn_s2 = espn_s2, swid = swid)
+#league = League(league_id = league_id, year = 2022, espn_s2 = espn_s2, swid = swid)
 
 def get_matchup_list(period):
     """
